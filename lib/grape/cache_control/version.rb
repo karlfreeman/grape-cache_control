25c009b678aa38b263f1b9e57c0f058d7533b3c5
Karl Freeman <karlfreeman@gmail.com>
2014-03-31 13:36:33 +0100

    bump

--- lib/grape/cache_control/version.rb
+++ lib/grape/cache_control/version.rb
@@ -1,5 +1,5 @@
 module Grape
   module CacheControl
-    VERSION = '0.0.1' unless defined?(Grape::CacheControl::VERSION)
+    VERSION = '0.0.2' unless defined?(Grape::CacheControl::VERSION)
   end
 end
